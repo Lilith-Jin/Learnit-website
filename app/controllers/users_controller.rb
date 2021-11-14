dc4950901e2998dde51d3f9043d73b4dbf93ca3d
root <root@LAPTOP-F86AFOMK.localdomain>
2021-11-14 08:53:39 +0800

    實體變數 params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ActionController::Base
   def sign_up
+    @user = User.new
   end
 
   def account_verify
-    #不需要出現畫面，快速轉址到首頁
-    redirect_to "/"
+   #限定(:username, :password, :email)是ok的
+    clean_params = params.require(:user).permit(:username, :password, :email)
+    @user = User.new(clean_params)
+
+    if @user.save
+    #快速轉址到首頁
+      redirect_to "/"
+    else
+    end
   end
 end
