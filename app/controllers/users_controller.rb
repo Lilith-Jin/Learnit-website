7e4ac46c3a75a59fb7680b47c05f679fb488a527
root <root@LAPTOP-F86AFOMK.localdomain>
2021-11-14 08:50:56 +0800

    creat sign_up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ActionController::Base
   def sign_up
   end
+
+  def account_verify
+    #不需要出現畫面，快速轉址到首頁
+    redirect_to "/"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     get "/", to: "pages#index"
     get "/about", to: "pages#about"
+
+    #user's action
     get "/sign_up", to: "users#sign_up"
+    post"/account_verify", to: "users#account_verify"
 end
