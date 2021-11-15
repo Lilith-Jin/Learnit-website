40eeaf421227db8a7ca5db4108f7c625fa806793
root <root@LAPTOP-F86AFOMK.localdomain>
2021-11-15 13:37:31 +0800

    驗證e-mail格式

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true
   #驗證email必填且為唯一值
-  validates :email, presence: true, uniqueness: true
+  # https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression  附上可用解答連結
+  validates :email, presence: true, uniqueness: true, format: { with: /^\S+@\S+\S.\S+$/, message: "only allows letters" }
 end
