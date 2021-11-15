664ec0bbc4d14f507fcf19fa9ba598ff3123d74e
root <root@LAPTOP-F86AFOMK.localdomain>
2021-11-15 22:31:59 +0800

    add  user sign in

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
     #user's action
     get "/sign_up", to: "users#sign_up"
     post"/account_verify", to: "users#account_verify"
+
+    get "/sign_in", to: "users#sign_in"
+    post "check", to: "users#check"
 end
