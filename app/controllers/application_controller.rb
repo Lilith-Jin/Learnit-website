e76cf0911b62a225c40590cd8b03668d5271ebfa
Lilith <li6253462@gmail.com>
2021-11-22 23:29:11 +0800

    check user status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  include UsersHelper
+    
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  # rescue_from ActiveRecord::RecordNotFound :with :record_not_found
+  # rescue_from 處理例外的方法 with: 要執行的方法
+  helper_method :user_signed_in?, :current_user
+  
+  private
+  def record_not_found
+    render file: 'public/404.html', layout: false, status: 404
+  end
+
+  def user_signed_in?
+    session[:recognize].present?
+  end
+
+  def current_user
+    @current_user || User.find(session[:recognize])
+  end
+end
 end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,18 +1,20 @@
 class CoursesController < ApplicationController
 
-  before_action :set_course, only:[:edit, :update, :destroy]
+  before_action :set_course, only: [:edit, :update, :destroy]
+  before_action :login?, except: [:index, :show]
 
   def index
     @courses = Course.all
   end
 
-  def
-    new
+  def new
     @course = Course.new
   end
 
   def create
-    @course = Course.new(course_params)
+    # @course = Course.new(course_params) 無判斷使用者狀態
+    @course = current_user.courses.build(course_params)
+
     if @course.save
       flash[:notice] = "課程新增成功"
       redirect_to courses_path
@@ -27,6 +29,7 @@ class CoursesController < ApplicationController
 
   def update
     @course = Course.find_by(id: params[:id])
+    
     if @course.update(course_params)
       flash[:notice] = "課程編輯成功"
       redirect_to courses_path
@@ -47,7 +50,27 @@ end
 private
 
 def set_course
-  @course = Course.find_by(id: params[:id])
+  # @course = Course.find_by(id: params[:id])
+  #目前使用者的id要跟課程id對上，否則會跳出例外訊息
+  begin
+    @course = current_user.courses.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render file: 'public/404.html', layout: false, status: 404
+    #layout: false ->不顯示flash和e-mail status: 404->預設會出現202，因為只是例外非錯誤
+    # redirect_to root_path
+    # flash[:notice] = "查無資料"
+
+  end
+end
+
+def login?
+  #如果沒登入，轉去登入頁面
+  # if not user_signed_in?
+  # if session[:recognize] == nil
+    # redirect_to sign_up_path
+  # end
+  redirect_to sign_in_path flash[:notice]='請先登入會員' unless user_signed_in?
+
 end
 
 def course_params
